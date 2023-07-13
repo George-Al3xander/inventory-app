6773d7832a20395cdacb679baaa6721f9d27fc25
George <george.valuiskyi@gmail.com>
2023-07-13 10:09:13 +0300

    Added 404 page

--- app.js
+++ app.js
@@ -46,21 +46,12 @@ app.get("/", (req, res) => {
 
 app.use('/catalog', indexRouter);
 app.use('/category', categoryRouter);
+app.use((req, res) => {
+  res.render("404", {
+    title: "404"
+  })
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 
 
